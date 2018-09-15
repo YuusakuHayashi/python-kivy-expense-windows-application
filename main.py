f0116dc234ff7b352fd36f001fc8c3d1ff571a24
yuusaku.hayashi <yuusaku.hayashi@outlook.jp>
2018-09-15 12:33:59 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from kivy.config import Config
+Config.set("graphics", "resizable", False)
+Config.set("graphics", "width", 640)
+Config.set("graphics", "height", 480)
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+USER_ID = "test"
+PASSWORD = "test"
+ERROR_MESSAGE = "Login Error"
+
+sm = ScreenManager()
+
+class LoginScreen(Screen):
+    def loginButtonClicked(self):
+        userID = self.ids["text_userID"].text
+        password = self.ids["text_password"].text
+        if userID == USER_ID and password == PASSWORD:
+            sm.current = "input"
+        else:
+            self.ids["error_message"].text = ERROR_MESSAGE
+class InputScreen(Screen):
+    def clearButtonClicked(self):
+        for key in self.ids:
+            self.ids[key].text = ""
+    def submitButtonClicked(self):
+        f = open("expense.txt","a",encoding="shift-jis")
+        sentence =""
+        for key in self.ids:
+            sentence += self.ids[key].text + " "
+        sentence += "\n"
+        f.write(sentence)
+        f.close()
+        #employeeID = self.ids["text_employeeID"].text
+        #dateofcost = self.ids["text_dateofcost"].text
+        #kindofcost = self.ids["text_kindofcost"].text
+        #valueofcost = self.ids["text_valueofcost"].text
+            
+class ExpenseApp(App):
+    def build(self):
+        sm.add_widget(LoginScreen(name="login"))
+        sm.add_widget(InputScreen(name="input"))
+        return sm
+
+if __name__ == '__main__':
+    ExpenseApp().run()
